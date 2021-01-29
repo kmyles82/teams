952df1d803727bd4c6857f484bf6f9648cce4fa1
kmyles82 <kmyles82@gmail.com>
2021-01-29 13:10:45 -0600

    setup new user account

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 import { ChatEngine } from 'react-chat-engine'
 
 import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="d48dd6ab-d98c-40cf-936f-9bb0ba696b4e"
+            userName="kmyles82"
+            userSecret="123123"
+        />
+    )
+}
+
+export default App
